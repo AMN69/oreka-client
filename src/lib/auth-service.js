568c53b842fa2347c66f94e660e4b51fe22d7fe1
Andreu Martínez <martinez.andreu@gmail.com>
2020-11-20 11:11:19 +0100

    [Sara+AMN] heroku version 1

--- src/lib/auth-service.js
+++ src/lib/auth-service.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
 class Auth {
+  // constructor() {
+  //   this.auth = axios.create({
+  //     baseURL: "http://localhost:4000",
+  //     withCredentials: true,
+  //   });
+  // }
   constructor() {
     this.auth = axios.create({
-      baseURL: "http://localhost:4000",
+      baseURL: process.env.REACT_APP_API_URL,
       withCredentials: true,
     });
-  }
+  };
 
   signup({ email, password, username, usersurname, age, userImgUrl }) {
     return this.auth
